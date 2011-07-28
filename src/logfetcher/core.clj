c6df5536e753380734ee6c0ca03ee7e72759aa3f
Erling <emmo@yousee.dk>
2011-07-28 09:04:49 +0200

    changes

--- src/logfetcher/core.clj
+++ src/logfetcher/core.clj
@@ -7,6 +7,6 @@
   "Return the web page as a string."
   [address]
   (let [url (URL. address)]
-    (with-open stream (. url (openStream))
+    (with-open [stream (. url (openStream))]
       (let [buf (BufferedReader. (InputStreamReader. stream))]
         (apply str (line-seq buf))))))
--- src/logfetcher/routes.clj
+++ src/logfetcher/routes.clj
@@ -1,19 +1,23 @@
 (ns logfetcher.routes
   (:use compojure.core 
         ring.util.response
-        clojure.contrib.json)
+        clojure.contrib.json
+	logfetcher.core)
   (:require [yousee-common.web :as common-web :only load-static-file]
 	    [compojure.route :as route]
 	    [compojure.handler :as handler]))
 
-
+;(def afsaetningurl "http://thenge.loggly.com/api/facets/date/?q=json.app:afsaetning%20-json.request.path:alive&from=NOW-7DAYS&until=NOW")
+(def afsaetningurl "http://localhost:41001/docs/afsaetning.json")
 
 (defroutes handler
 
   (GET ["/:context/ping", :context #".[^/]*"]  []
        {:status 200, :headers {"Content-Type" "text/plain" "expires" "0" "cache-control" "no-cache"} :body "pong" })
 
-
+  (GET ["/:context/afsaetning", :context #".[^/]*"] []
+       (prn (:data (read-json (fetch-url afsaetningurl)))))
+  
   (GET ["/:context/public/:fil" , :context #".[^/]*" :fil #".*\..*"] [fil]
        (common-web/load-static-file fil))
 
